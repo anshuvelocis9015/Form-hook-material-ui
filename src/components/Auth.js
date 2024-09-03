0d415c5ff745c73f486b86e738f184e524cfb761
Anshu Kumar <anshu.kumar@velocis.in>
2024-09-03 18:45:00 +0530

    adding auth

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 
+// const Auth = () => {
+//     const {handleSubmit,setValue} = useForm();
+//     const[phoneNumber,setPhoneNumber] = useState("");
+//     const onSubmit = (data) =>{
+//         console.log(data);
+//     }
+//     const handleChange = (e) => {
+//         setPhoneNumber(e.target.value);
+//         setValue(phoneNumber);
+//     }
+//     return(
+//         <div>
+//         <h1>Auth</h1>
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//         <input 
+//         type='tel' 
+//         placeholder='+911234567890'
+//         name='phoneNumber'
+//         value={phoneNumber}
+//         onChange={handleChange}
+//         // {...register('phoneNumber',{
+//         //     required:'phone number is required'
+//         // })}
+//         />
+//         <button type='submit'>Submit</button>
+//         </form>
+//         </div>
+//     )
+// }
 const Auth = () => {
-    const {handleSubmit,setValue} = useForm();
-    const[phoneNumber,setPhoneNumber] = useState("");
+    const {register,handleSubmit,formState:{errors}} = useForm();
     const onSubmit = (data) =>{
         console.log(data);
     }
-    const handleChange = (e) => {
-        setPhoneNumber(e.target.value);
-        setValue(phoneNumber);
-    }
+    // const onFormError = (error) => console.log(error);
     return(
         <div>
         <h1>Auth</h1>
@@ -20,14 +45,18 @@ const Auth = () => {
         <input 
         type='tel' 
         placeholder='+911234567890'
-        name='phoneNumber'
-        value={phoneNumber}
-        onChange={handleChange}
-        // {...register('phoneNumber',{
-        //     required:'phone number is required'
-        // })}
+        {...register('phoneNumber',{
+            pattern:{
+            value:/[a-zA-z]{3}/,
+            message:"invalid phone number",
+            },
+             required:"phone number is required"
+        }
+        
+    )}
         />
-        <button type='submit'>Submit</button>
+        {errors?.phoneNumber&&<span>{errors.phoneNumber.message}</span>}
+        <button type='submit'>Send OTP</button>
         </form>
         </div>
     )
